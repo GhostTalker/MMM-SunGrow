b707027eba10db588500b72c78762db0c112a853
GhostTalker <49254289+GhostTalker@users.noreply.github.com>
2025-01-08 21:11:47 +0100

    get basic plant info

--- node_helper.js
+++ node_helper.js
@@ -131,6 +131,7 @@ module.exports = NodeHelper.create({
         },
         body: JSON.stringify(body)
       });
+      console.log("[MMM-SunGrow] /openapi/login response:", loginData);
 
       if (!res.ok) {
         // Possibly the token expired? We might need to re-login.
